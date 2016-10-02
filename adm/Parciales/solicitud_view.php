66c99de8bc74e8a97a682745ce2da796c1a9c736
marcos <marcos@marcos-desktop>
2016-10-01 23:17:14 -0430

    varios

--- adm/Parciales/solicitud_view.php
+++ adm/Parciales/solicitud_view.php
@@ -1,15 +1,15 @@
 
 	<div class="col-sm-12 col-md-12" >
-		<div class="col-sm-3 col-md-3">
+		<div class="col-sm-3 col-md-4">
 			<pre><label>Funcionario:</label> <?php echo $data['first_name'];?> <?php echo $data['first_last_name'];?></pre>
 		</div>
-		<div class="col-sm-3 col-md-3">
+		<div class="col-sm-3 col-md-4">
 			<pre><label>Ubicación administrativa:</label> <?php echo $data['nom_ubi'];?></pre>
 		</div>
-		<div class="col-sm-3 col-md-3">
+		<div class="col-sm-3 col-md-2">
 			<pre><label>Fecha de solicitud:</label> <?php echo $data['fec_solicitud'];?></pre>
 		</div>
-		<div class="col-sm-3 col-md-3">
+		<div class="col-sm-3 col-md-2">
 			<pre><label>Fecha de reservación:</label> <?php echo $data['fec_reservacion'];?></pre>
 		</div>
 	</div>
